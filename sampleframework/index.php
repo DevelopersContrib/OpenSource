0ed5b32a2a6ba0f3b65a0e982dc7a47c52b1a8e0
Chikaysci <kjabellar@gmail.com>
2013-05-16 14:25:33 -0700

    sample framework initial files

---
+++ sampleframework/index.php
@@ -0,0 +1,76 @@
+<? include('header.php');?>
+
+	<div id="adbox">
+		<div class="body">
+			<div class="images">
+				<img src="images/discussing2.jpg" alt="Img" class="preview" />
+				<img src="images/shake-hands.jpg" alt="Img" height="144" width="230px" />
+				<img src="images/professionals.jpg" alt="Img" height="135" width="230px" class="last" />
+			</div>
+			<div class="details">
+				<p>
+					<span>
+						Lorem ipsum dolor sit amet, <a href="#">Fconsectetur adipisicing</a>  elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</span>
+					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				</p>
+			</div>
+		</div>
+	</div> <!-- /#adbox -->
+	<div id="contents" style="padding: 1px 0 20px 0;">
+		<div class="body" style="margin-top:-40px">
+			<div id="sidebar">
+				<h3>Blog</h3>
+				<ul>
+					<li>
+						Quisque iaculis erat in velit fermentum pellentesque. 
+						<span>Sept. 21 | by Nullam <a href="blog.php">8</a></span>
+					</li>
+					<li>
+						Semper. Cras eu elit velit. Nullam vel eros turpis.
+						<span>Sept. 21 | by Nullam <a href="blog.php">8</a></span>
+					</li>
+					<li>
+						Aenean id erat elit, quis venenatis erat. 
+						Cras ornare orci vitae metus 
+						<span>Sept. 21 | by Nullam <a href="blog.php">8</a></span>
+					</li>
+					<li>
+						Vivamus euismod dictum purus ac semper. 
+						Etiam sed elit id ante commodo
+						<span>Sept. 21 | by Nullam <a href="blog.php">8</a></span>
+					</li>
+					<li>
+						Fusce ac quam nibh, at pharetra dolor. 
+						In quis odio arcu, vel laoreet eros. 
+						Nam vel sem dui. 
+						<span>Sept. 21 | by Nullam <a href="blog.php">8</a></span>
+					</li>
+				</ul>
+			</div>
+			<div id="main">
+				<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
+				<ul>
+					<li>
+						<a href=""><img src="images/globe.jpg" alt="Img" /><h3>Lorem ipsum dolor sit amet</h3></a>
+						<p>Donec venenatis, mauris in blandit tempor, sem tellus vehicula lorem</p>
+					</li>
+					<li>
+						<a href=""><img src="images/tools.jpg" alt="Img" /><h3>Proin condiment porttitor ultrices</h3></a>
+						<p>Cras tristique faucibus sagittis. Praesent pulvinar, dolor in posuere vulputate</p>
+					</li>
+					<li>
+						<a href=""><img src="images/check.jpg" alt="Img" /><h3>Quisque consequat</h3></a>
+						<p>Sed vitae massa at turpis mollis aliquam sed vitae tellus integer arcu</p>
+					</li>
+					<li>
+						<a href=""><img src="images/graph.jpg" alt="Img" /><h3>Class aptent taciti sociosqu</h3></a>
+						<p>Nam quis arcu erat ultricies volutpat leo, in molestie est laoreet eu</p>
+					</li>
+				</ul>
+				
+			</div>
+		</div>
+	</div> <!-- /#contents -->
+	
+<? include('footer.php');?>
